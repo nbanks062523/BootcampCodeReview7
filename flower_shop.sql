9ae848a8d3b1d323cdc99b335f39ab9e6939a241
nbanks062523 <nbanks@kingsleytechpartners.com>
2023-12-17 08:48:31 -0500

    updated sql file and added python code to show newly added table

--- flower_shop.sql
+++ flower_shop.sql
@@ -1,10 +1,9 @@
-CREATE TABLE 'flower_shop' (
-    'plant_id' INT(10) NOT NULL,
-    'species_name' VARCHAR(256) NOT NULL,
-    'variety_name' VARCHAR(256) NULL,
-    'zone' INT(1) NULL,
-    'price' DECIMAL(6,2) NULL,
-    PRIMARY KEY ('plant_id')
+CREATE TABLE plants.flower_shop (
+    plant_id INTEGER NOT NULL,
+    species_name STRING(256) NOT NULL,
+    variety_name STRING(256),
+    zone INTEGER,
+    price DECIMAL(6,2),
 );
-INSERT INTO flower_shop (plant_id,species_name,variety_name,zone,price)
+INSERT INTO plants.flower_shop (plant_id,species_name,variety_name,zone,price)
 VALUES(1,'monstera','swiss_cheese',5,20.25),(2,'cactus','star',8,8.00),(3,'cactus','bunny_ear',9,8.00),(4,'fern','birds_nest',6,12.50),(5,'cactus','saguaro',8,8.00),(6,'fern','giant',7,11.00),(7,'monstera','albo',5,14.75),(8,'cactus','blue_columnar',8,9.00),(9,'cactus','barrel',9,14.50),(10,'bromeliad','guzmania',9,15.50);
